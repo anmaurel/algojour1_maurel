4738a98aa08fb163bc178fa40f10c39488f0f6f8
anmaurel <antoinemaurel6@gmail.com>
2021-01-20 14:15:24 +0100

    :up: Update API and UI

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,6 +1,12 @@
 from hashlib import sha256
 from datetime import datetime
+from tkinter import *
+from flask import Flask
+import json
 
+"""
+Block
+"""
 class Block:
 
     def __init__(self, index, previous_hash, timestamp, data, nonce):
@@ -15,7 +21,9 @@ class Block:
         block = str(self.index) + str(self.previous_hash) + str(self.timestamp) + str(self.data) + str(self.nonce)
         return sha256(block.encode('utf-8')).hexdigest()
 
-
+"""
+Blockchain
+"""
 class Blockchain: 
     number_of_zero = 4
 
@@ -24,11 +32,25 @@ class Blockchain:
         self.add_first_block()
     
     def get_blocks(self):
-        return self.blocks
+        blocks = {}
+        blocks['block'] = []
+        for block in self.blocks:
+            blocks['block'].append(
+                {
+                    'index' : block.index,
+                    'previous hash' : block.previous_hash,
+                    'timestamp' : block.timestamp,
+                    'data' : block.data,
+                    'hash' : block.hash,
+                    'nonce' : block.nonce,
+                }
+            )
+
+        return blocks
  
     def add_first_block(self):
         first_block = Block(0, '0', datetime.now(), 'Genesis block', '0')
-        first_block.hash = first_block.do_hash()
+        self.correct_hash(first_block)
         self.blocks.append(first_block)
 
     def new_block(self, data):
@@ -46,6 +68,9 @@ class Blockchain:
         self.correct_hash(block)
         self.blocks.append(block)
 
+        with open('blocks.json', 'w') as push:
+            json.dump(push)
+
     def correct_hash(self, block):
         exec_hash = block.do_hash()
         block.nonce = 0
@@ -56,23 +81,63 @@ class Blockchain:
 
         block.hash = exec_hash
     
+    def submit(): 
+        input_block = input.get() 
+        print("The data is : " + input_block) 
 
 bchain = Blockchain()
 
-block_n1 = bchain.new_block("Second Block")
-bchain.add_block(block_n1)
 
-block_n2 = bchain.new_block("Third Block")
-bchain.add_block(block_n2)
+"""
+API Flask
+"""
+# app = Flask(__name__)
 
-block_n3 = bchain.new_block("Fourth Block")
-bchain.add_block(block_n3)
+# @app.route('/', methods=['GET'])
+# def get_all_blocks():
+#     return json.dumps(bchain.get_blocks(), sort_keys = True, default = str, indent = 2)
 
-for block in bchain.get_blocks():
-    print("index : ", block.index)
-    print("previous hash : ", block.previous_hash)
-    print("timestamp : ", block.timestamp)
-    print("data : ", block.data)
-    print("hash : ", block.hash)
-    print("nonce : ", block.nonce)
-    print("\n")
+# app.run(debug = True, port = 5000)
+
+"""
+User Interface
+"""
+ui = Tk()
+
+ui.title('Blockchain IIM')
+ui.geometry("600x600+20+20")
+
+label = Label(ui, text="Nom de block :")
+label.pack()
+input = Entry(ui) 
+input.pack()
+input.focus_set()
+
+def return_blocks():
+    block = bchain.new_block(input.get())
+    bchain.add_block(block)
+
+    results.config(text = format_results())
+    input.delete(0,END)
+
+def format_results():
+    blocks = []
+    for block in bchain.get_blocks():
+        blocks.append([
+            "index : " + str(block.index) + "",
+            block.previous_hash,
+            block.timestamp,
+            block.data,
+            block.hash,
+            block.nonce,
+        ])
+
+    return blocks
+
+button = Button(ui, text = "Créer", width = 10, command = return_blocks)
+button.pack()
+
+results = Label(ui, height = 100, width = 600, wraplength = 600)
+results.pack()
+
+ui.mainloop()
